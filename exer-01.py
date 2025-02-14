10c308d45c4b57efb419d40d75e2176a7438ccdd
ayaelzein <aya.m.elzein@gmail.com>
2025-02-14 13:31:16 +0200

    price for elderly

--- exer-01.py
+++ exer-01.py
@@ -2,5 +2,7 @@ age = int(input("Please enter your age: "))
 
 if age <= 10:
     print("Your total will be 5 dollars")
-elif age > 10:
+elif 10 < age < 70:
     print("Your total will be 10 dollars")
+elif age >= 70:
+    print ("You'll get the early bird discount, so your total will be 5 dollars")
